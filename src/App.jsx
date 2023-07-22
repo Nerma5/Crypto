22236249a45cffe8d50fa9b5fe5a8895f08478de
Edin Mavric <edinmavric10@gmail.com>
2023-07-22 03:32:00 +0200

    Added navigation for whole website

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-import './App.scss'
-import Home from './pages/Home/index'
-import Fav from './pages/Fav/index'
-import Nav from './components/Nav/index'
-import Footer from './components/Footer/index'
-import { Route, Routes } from 'react-router-dom'
+import './App.scss';
+import Home from './pages/Home/index';
+import Fav from './pages/Fav/index';
+import Nav from './components/Nav/index';
+import Footer from './components/Footer/index';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Nav />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/fav' element={<Fav />} />
-        <Route  />
+        <Route path="/" element={<Home />} />
+        <Route path="/fav" element={<Fav />} />
+        <Route />
       </Routes>
-      Hello
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,20 +1,36 @@
-import React from 'react'
-import './index.scss'
-import { NavLink } from 'react-router-dom'
-import { Favorite } from "@mui/icons-material";
+import React from 'react';
+import './index.scss';
+import { NavLink } from 'react-router-dom';
+import { Favorite } from '@mui/icons-material';
+import { Avatar } from '@mui/material';
 
 const Nav = () => {
   return (
-    <div className='Nav'>
+    <div className="Nav">
       <div className="img">
-        IMG
+        <img
+          width="40"
+          src="https://i.postimg.cc/8P0LjnXR/logo-removebg-preview.png"
+          alt=""
+        />
+        CRYPTO-APP
       </div>
       <div className="links">
-        <NavLink to='/'>Home</NavLink>
-        <NavLink to='/fav'><Favorite /></NavLink>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/coins">Coins</NavLink>
+        <NavLink to="/exchanges">Exchanges</NavLink>
+        <NavLink to="/about">About us</NavLink>
+        <NavLink to="/fav">
+          <Favorite />
+        </NavLink>
+      </div>
+      <div className="profile">
+        <NavLink to="/profile">
+          <Avatar />
+        </NavLink>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
