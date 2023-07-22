f287004165b6f2344647ff0c8683b2d5e2b633f3
Edin Mavric <edinmavric10@gmail.com>
2023-07-22 03:41:48 +0200

    Added Working Routes for the whole Website

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import './App.scss';
 import Home from './pages/Home/index';
 import Fav from './pages/Fav/index';
+import Profile from './pages/Profile/index';
+import Coins from './pages/Coins/index';
+import Exchanges from './pages/Exchanges/index';
+import About from './pages/About/index';
 import Nav from './components/Nav/index';
 import Footer from './components/Footer/index';
 import { Route, Routes } from 'react-router-dom';
@@ -12,7 +16,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/fav" element={<Fav />} />
-        <Route />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/coins" element={<Coins />} />
+        <Route path="/exchanges" element={<Exchanges />} />
+        <Route path="/about" element={<About />} />
       </Routes>
       <Footer />
     </div>
---
+++ src/pages/Coins/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './index.scss';
+
+const Coins = () => {
+  return <div className='Coins'>Coins</div>;
+};
+
+export default Coins;
