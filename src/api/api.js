86c860bf42230a77217d6225b25dd69bb24ff348
umersacirovic2006 <asacirovic90@gmail.com>
2023-07-24 01:33:46 +0200

    Added context and axios create instance that may be used later

--- src/api/api.js
+++ src/api/api.js
@@ -1,2 +1,12 @@
 // I was thinking of having all of them in a separent folder
 // one for axios create and then all of the other ones
+
+import axios from 'axios';
+
+export const coinRankingAPI = axios.create({
+  baseURL: 'https://coinranking1.p.rapidapi.com',
+  headers: {
+    'X-RapidAPI-Key': 'cca8a610e8msh3412e54e4496bf1p1c57f2jsna0d7515e9c32',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+  }
+});
